50b53cda3bda9afec4b7dd2d63f0d6f05c4ecdbd
kritika.dutt.upadhyay kritika.dutt.upadhyay <kdutt1@uncc.edu>
2020-10-01 15:02:22 -0400

    Adding server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget ={
+    myBudget: [
+    {
+        title: 'Shopping',
+        budget: 50
+    },
+
+    {
+        title: 'rent',
+        budget: 400
+    },
+    {
+        title: 'groceries',
+        budget: 80
+    },
+
+]
+};
+
+
+app.get('/kkk', (req, res) => {
+    res.send('Hello World!');
+
+});
+
+
+app.get('/budget', (req, res) => {
+res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
