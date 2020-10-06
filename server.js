f76160c3039c16cc0309406b5d56e4ec24277bae
kritika.dutt.upadhyay kritika.dutt.upadhyay <kdutt1@uncc.edu>
2020-10-06 17:16:59 -0400

    Adding cors call back function

--- server.js
+++ server.js
@@ -3,58 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-//var budget = require('./Kritika.json');
+var budget = require('./Kritika.json');
+
 app.use(cors());
 
-
-//app.use('/', express.static('public')); 
-
-const budget ={
-    myBudget: [
-    {
-        title: 'rent',
-        budget: 400
-    },
-
-    {
-        title: 'groceries',
-        budget: 80
-    },
-    {
-        title: 'shopping',
-        budget: 200
-    },
-    {
-        title: 'Beauty & wellness',
-        budget: 150
-    },
-    {
-        title: 'Dine in',
-        budget: 100
-    },
-    {
-        title: 'Travel',
-        budget: 80
-    },
-    {
-        title: 'Education',
-        budget: 100
-    }
-]
-}; 
-
-
-
-/*app.get('/kkk', (req, res) => {
-    res.send('Hello World!');
-
-}); */
-
-
-app.get('/budget', (req, res) => {
+app.get('/budget',cors(), (req, res) => {
+    console.log("budget");
 res.json(budget);
 });
-
 
 
 app.listen(port, () => {
